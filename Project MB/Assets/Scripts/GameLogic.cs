fe71c01824504d05218846f73e1489f879834f95
JK <lexuskrav@gmail.com>
2021-07-04 21:03:08 +0800

    GameLogic

--- Project MB/Assets/Scripts/GameLogic.cs
+++ Project MB/Assets/Scripts/GameLogic.cs
@@ -8,29 +8,37 @@ public class GameLogic : MonoBehaviour
     [SerializeField] bool isActiveRock;
     [SerializeField] List<GameObject> Rock;
 
+    Camera mainCamera;
     float rockHoldTime;
-
+    Rigidbody rockRB;
     Transform rockSpawnPoint;
 
     void RockSpawner()
     {
         // Make rocks fall in a certain position
         // Choose from the list of rocks
+        // randomize rock selection
+
         Random.Range(0, Rock.Count);
 
+        rockRB = GetComponent<Rigidbody>();
     }
 
     void RockHold()
     {
         //Hold for a certain amount of time to balance rock
         //disable rb after holding
-
+        if(rockHoldTime <= 0)
+        {
+            rockRB.isKinematic = true; // disables the physics part
+            //rockspawn then reset rock hold time
+        }
     }
 
     void CameraLock()
     {
         //increment y transform of camera per rock spawned
-        //
+        //call when rockspawn is called
     }
 
     private void Update()
